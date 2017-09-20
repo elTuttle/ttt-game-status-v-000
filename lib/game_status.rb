22505bfa62367c504d856a2516cfe8acfb74eb11
John Tuttle <thejwt@gmail.com>
2017-09-20 19:39:02 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,21 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+# Define your WIN_COMBINATIONS const
+WIN_COMBINATIONS = [
+  [0,1,2]
+  [3,4,5]
+  [6,7,8]
+  [0,3,6]
+  [1,4,7]
+  [2,5,8]
+  [0,4,8]
+  [2,4,6]
+] 
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combos|
+    win_combos.detect{|i|}
+  end
+  
+end
